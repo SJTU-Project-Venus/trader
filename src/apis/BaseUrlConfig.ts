fa5a00721228bbb793946064dc2416be9b168cdb
SAIL <1127858301@qq.com>
2020-06-14 16:48:46 +0800

    改url

--- src/apis/BaseUrlConfig.ts
+++ src/apis/BaseUrlConfig.ts
@@ -1,3 +1,3 @@
-export const TRADER_BACKEND = 'http://trader-gateway.traderone:8080'; //'http://localhost:8081';
+export const TRADER_BACKEND = 'http://trader-gateway:8080'; //'http://localhost:8081';
 export const BROKER_GATEWAY = 'http://broker-gateway.brokerone:8080'; //'http://localhost:8080';
-export const TRADER_WEBSOCKET = 'ws://trader-gateway.traderone:8080/ws'; //'ws://localhost:8081/ws';
+export const TRADER_WEBSOCKET = 'ws://trader-gateway:8080/ws'; //'ws://localhost:8081/ws';
