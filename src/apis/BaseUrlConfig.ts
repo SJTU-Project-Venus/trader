b2fdfe3d98e789aea7928ba8ce3dbca70e3f668c
SAIL <1127858301@qq.com>
2020-06-14 23:22:17 +0800

    更改url为云端

--- src/apis/BaseUrlConfig.ts
+++ src/apis/BaseUrlConfig.ts
@@ -1,2 +1,2 @@
-export const TRADER_BACKEND = 'http://localhost:8082'; //'http://202.120.40.8:30383'; //'http://trader-gateway:8080'; //
-export const TRADER_WEBSOCKET = 'ws://localhost:8082/ws'; //'ws://202.120.40.8:30383/ws';
+export const TRADER_BACKEND = 'http://202.120.40.8:30383'; //'http://localhost:8082';
+export const TRADER_WEBSOCKET = 'ws://202.120.40.8:30383/ws'; //'ws://localhost:8082/ws';
